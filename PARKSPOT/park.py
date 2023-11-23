2eac83489f264825fc4b4b3a7adf3599f8d74a8c
Tanay <127974995+Tanay2920003@users.noreply.github.com>
2023-11-23 18:17:08 +0530

    Update park.py

--- PARKSPOT/park.py
+++ PARKSPOT/park.py
@@ -1,44 +1,68 @@
 import random
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy_garden.mapview import MapView
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy_garden.mapview import MapView, MapMarker
 from kivy.uix.label import Label
 from mapbox import Geocoder
+from kivy.uix.anchorlayout import AnchorLayout
 
 class ParkApp(App):
     def build(self):
-        # Generate random latitude and longitude values
-        latitude = random.uniform(-90, 90)
-        longitude = random.uniform(-180, 180)
-
         # Initialize the Mapbox geocoder
-        geocoder = Geocoder(access_token='sk.eyJ1IjoidGFuYXkyMiIsImEiOiJjbHBiMjM3MmgwZzNtMmpua3VxdzB6MnYwIn0.y9XcBNPoA6TuMMp5VuEEQw')
+        self.geocoder = Geocoder(access_token='sk.eyJ1IjoidGFuYXkyMiIsImEiOiJjbHBiMjM3MmgwZzNtMmpua3VxdzB6MnYwIn0.y9XcBNPoA6TuMMp5VuEEQw')
 
-        # Reverse geocode the location to get the address
-        response = geocoder.reverse(lon=longitude, lat=latitude)
+        # Create a label with the address
+        self.label = Label()
 
-        # Print the response for debugging
-        print(response.json())
+        # Create a MapView
+        self.mapview = MapView(zoom=15)
 
-        # Get the formatted address from the response
-        try:
-            address = response.json()['features'][0]['place_name']
-        except IndexError:
-            address = "Address not found"
+        # Create a MapMarker
+        self.marker = MapMarker()
+        self.mapview.add_widget(self.marker)
 
-        # Create a label to display the address
-        label = Label(text=address)
-
-        # Create a map view
-        mapview = MapView(zoom=15, lat=latitude, lon=longitude)
-
-        # Create a layout to hold the label and map view
+        # Create a vertical BoxLayout
         layout = BoxLayout(orientation='vertical')
-        layout.add_widget(label)
-        layout.add_widget(mapview)
+
+        # Create a horizontal BoxLayout for the search box and button
+        search_layout = BoxLayout(orientation='horizontal', size_hint=(0.8, None))
+
+        # Create a search box and add it to the search layout
+        self.search_box = TextInput(hint_text='Enter address', size_hint=(0.8, 1))
+        search_layout.add_widget(self.search_box)
+
+        # Create a search button and add it to the search layout
+        search_button = Button(text='Search', size_hint=(0.2, 1))
+        search_button.bind(on_press=self.search_address)
+        search_layout.add_widget(search_button)
+
+        # Create an AnchorLayout and add the search layout to it
+        anchor_layout = AnchorLayout(anchor_x='center', anchor_y='center')
+        anchor_layout.add_widget(search_layout)
+
+        # Add the AnchorLayout to the main layout
+        layout.add_widget(anchor_layout)
+
+        # Add the label and MapView to the layout
+        layout.add_widget(self.label)
+        layout.add_widget(self.mapview)
 
         return layout
 
-# Run the app
+    def search_address(self, instance):
+        address = self.search_box.text
+        response = self.geocoder.forward(address)
+        try:
+            feature = response.json()['features'][0]
+            self.mapview.lat = feature['center'][1]
+            self.mapview.lon = feature['center'][0]
+            self.marker.lat = feature['center'][1]
+            self.marker.lon = feature['center'][0]
+            self.label.text = feature['place_name']
+        except IndexError:
+            self.label.text = "Address not found"
+
 if __name__ == '__main__':
     ParkApp().run()
