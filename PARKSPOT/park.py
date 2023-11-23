f718dd671ffbac255c4a70a4aa7fcac9e97f506a
Tanay <127974995+Tanay2920003@users.noreply.github.com>
2023-11-23 17:41:41 +0530

    Update park.py

--- PARKSPOT/park.py
+++ PARKSPOT/park.py
@@ -1,17 +1,20 @@
+import random
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy_garden.mapview import MapView
 from kivy.uix.label import Label
 from mapbox import Geocoder
 
 class ParkApp(App):
     def build(self):
-        # Get the user's location using the GPS (Note: This won't work on a non-Android device)
-        latitude = 0.0
-        longitude = 0.0
+        # Generate random latitude and longitude values
+        latitude = random.uniform(-90, 90)
+        longitude = random.uniform(-180, 180)
 
         # Initialize the Mapbox geocoder
         geocoder = Geocoder(access_token='sk.eyJ1IjoidGFuYXkyMiIsImEiOiJjbHBiMjM3MmgwZzNtMmpua3VxdzB6MnYwIn0.y9XcBNPoA6TuMMp5VuEEQw')
 
-        # Reverse geocode the user's location to get the address
+        # Reverse geocode the location to get the address
         response = geocoder.reverse(lon=longitude, lat=latitude)
 
         # Print the response for debugging
@@ -26,7 +29,15 @@ class ParkApp(App):
         # Create a label to display the address
         label = Label(text=address)
 
-        return label
+        # Create a map view
+        mapview = MapView(zoom=15, lat=latitude, lon=longitude)
+
+        # Create a layout to hold the label and map view
+        layout = BoxLayout(orientation='vertical')
+        layout.add_widget(label)
+        layout.add_widget(mapview)
+
+        return layout
 
 # Run the app
 if __name__ == '__main__':
