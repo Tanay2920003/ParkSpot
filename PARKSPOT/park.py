0b50cc88c63cb184235c31cbe9648a8fabd90e9a
Tanay <127974995+Tanay2920003@users.noreply.github.com>
2023-11-23 18:21:14 +0530

    Update park.py

--- PARKSPOT/park.py
+++ PARKSPOT/park.py
@@ -6,15 +6,12 @@ from kivy.uix.button import Button
 from kivy_garden.mapview import MapView, MapMarker
 from kivy.uix.label import Label
 from mapbox import Geocoder
-from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.floatlayout import FloatLayout
 
 class ParkApp(App):
     def build(self):
         # Initialize the Mapbox geocoder
         self.geocoder = Geocoder(access_token='sk.eyJ1IjoidGFuYXkyMiIsImEiOiJjbHBiMjM3MmgwZzNtMmpua3VxdzB6MnYwIn0.y9XcBNPoA6TuMMp5VuEEQw')
-
-        # Create a label with the address
-        self.label = Label()
 
         # Create a MapView
         self.mapview = MapView(zoom=15)
@@ -23,11 +20,11 @@ class ParkApp(App):
         self.marker = MapMarker()
         self.mapview.add_widget(self.marker)
 
-        # Create a vertical BoxLayout
-        layout = BoxLayout(orientation='vertical')
+        # Create a FloatLayout
+        layout = FloatLayout()
 
         # Create a horizontal BoxLayout for the search box and button
-        search_layout = BoxLayout(orientation='horizontal', size_hint=(0.8, None))
+        search_layout = BoxLayout(orientation='horizontal', size_hint=(0.8, 0.1), pos_hint={'top': 1})
 
         # Create a search box and add it to the search layout
         self.search_box = TextInput(hint_text='Enter address', size_hint=(0.8, 1))
@@ -38,16 +35,9 @@ class ParkApp(App):
         search_button.bind(on_press=self.search_address)
         search_layout.add_widget(search_button)
 
-        # Create an AnchorLayout and add the search layout to it
-        anchor_layout = AnchorLayout(anchor_x='center', anchor_y='center')
-        anchor_layout.add_widget(search_layout)
-
-        # Add the AnchorLayout to the main layout
-        layout.add_widget(anchor_layout)
-
-        # Add the label and MapView to the layout
-        layout.add_widget(self.label)
+        # Add the MapView and search layout to the FloatLayout
         layout.add_widget(self.mapview)
+        layout.add_widget(search_layout)
 
         return layout
 
@@ -60,9 +50,8 @@ class ParkApp(App):
             self.mapview.lon = feature['center'][0]
             self.marker.lat = feature['center'][1]
             self.marker.lon = feature['center'][0]
-            self.label.text = feature['place_name']
         except IndexError:
-            self.label.text = "Address not found"
+            print("Address not found")
 
 if __name__ == '__main__':
     ParkApp().run()
